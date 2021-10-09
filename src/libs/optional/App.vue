9a8fc0edb218bb20924d447c152e6cee6e9179f4
yeqisong <445287302@qq.com>
2021-10-09 09:01:07 +0800

    add options.html demo

---
+++ src/libs/optional/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <img src="@/assets/images/logo.png" alt="">
+        <HelloPopup />
+    </div>
+</template>
+<script setup>
+import HelloPopup from './components/HelloPopup.vue'
+</script>
+<style scoped></style>
